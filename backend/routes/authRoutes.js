86a0136541557c0c9d289daadc7303aefc54fd2d
Ayesha Dawodi <166828984+dlawiz83@users.noreply.github.com>
2025-11-05 14:52:49 +0430

    Fix bugs

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const { protect } = require("../middleware/authMiddleware"); 
 const { registerUser, loginUser, updateProfile, changePassword } = require('../controllers/authController');
 
 router.post('/signup' , registerUser);
